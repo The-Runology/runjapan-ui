f89b1db6dd0db4c186ab44683a53752c94aa7ea0
sunshinePlusIce <sunshineplusice@gmail.com>
2023-01-29 01:27:00 +0800

    update homepage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Topbar/>
-  <div></div>
+  <HomeView />
   <Footer />
 </template>
 
@@ -8,10 +8,12 @@
 <script lang="ts">
 import Topbar from './components/general/Topbar.vue';
 import Footer from './components/general/Footer.vue';
+import HomeView from '@/views/HomeView.vue';
 export default {
   components: {
     Topbar,
-    Footer
+    Footer,
+    HomeView
   }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,15 @@
-
 <template>
-<div class="bg-white bg">
-  <div class="button-area d-flex justify-content-evenly">
-    <Button class="run-btn bg-red run" text="一鍵跑路🏃" fontSize="35px"></Button>
-  </div>
-  <div class="suggest d-flex flex-row">
-    
-  </div>
-</div> 
+<div class="home">
+  <Suggest />
+</div>
 </template>
 
 <script lang="ts">
-import Button from '../components/general/Button.vue';
+import Suggest from '@/components/features/Suggest.vue'
+
 export default {
   components: {
-    Button
+    Suggest
   }
 }
 </script>
-
-<style scoped>
-div.bg {
-  width: 100%;
-  height: 1000px;
-}
-
-</style>
